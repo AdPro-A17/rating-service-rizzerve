44f77c745f82e5d563e1db8e5ae69d0a0e7cabb7
Kevin Manurung <kevinmanurung0207@gmail.com>
2025-04-11 20:35:04 +0700

    [GREEN] implement model

---
+++ src/main/java/id/ac/ui/cs/advprog/rating_service/model/RatingModel.java
@@ -0,0 +1,41 @@
+package id.ac.ui.cs.advprog.rating_service.model;
+
+public class RatingModel {
+    private String id;
+    private String userId;
+    private String menuId;
+    private int score;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(String menuId) {
+        this.menuId = menuId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
+
