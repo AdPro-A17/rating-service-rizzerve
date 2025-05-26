d50351283e59fc097f9d5dcb4b32d9461100c909
Kevin Manurung <kevinmanurung0207@gmail.com>
2025-05-26 23:59:11 +0700

    deploying dev branch

--- src/main/java/id/ac/ui/cs/advprog/rating_service/config/CorsConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/rating_service/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("${FRONTEND_URL}:http://localhost:3000"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         corsConfiguration.setAllowCredentials(true);
