c20b663dedbd7b08dbc3ac7396dfba1a6469ec65
KayeyeY <110380893+KayeyeY@users.noreply.github.com>
2025-05-27 00:20:14 +0700

    Revert "Dev"

--- src/main/java/id/ac/ui/cs/advprog/rating_service/exception/RatingNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advprog.rating_service.exception;
-
-public class RatingNotFoundException extends RuntimeException {
-    public RatingNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/id/ac/ui/cs/advprog/rating_service/observer/RatingObserver.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.rating_service.observer;
-
-import java.util.UUID;
-
-public interface RatingObserver {
-    void updateRating(UUID itemId, double averageRating);
-}
-
